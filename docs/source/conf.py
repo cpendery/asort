c927cdb6fc2c76395b5c0ade346d92478e4af89c
cpendery <cpendery@vt.edu>
2022-06-10 15:36:39 +0200

    docs: add sphinx docs

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
+with open("../../VERSION.txt", encoding="utf-8") as file:
+    version = file.readline().strip()
+
+# -- Project information
+
+project = "asort"
+copyright = "2022-Present, cpendery"  # pylint: disable=redefined-builtin
+author = "cpendery"
+
+# -- General configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ["_templates"]
+
+# -- Options for HTML output
+
+html_theme = "sphinx_rtd_theme"
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
---
+++ docs/source/index.rst
@@ -0,0 +1,16 @@
+Welcome to asort's documentation!
+===================================
+
+**asort** is a Python library and command line utility for ensuring all
+*__all__* lists in python are kept sorted
+
+Check out the :doc:`usage` section for further information, including
+how to :ref:`installation` the project.
+
+Contents
+--------
+
+.. toctree::
+
+   usage
+   api
